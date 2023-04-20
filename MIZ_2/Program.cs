af090f6d9c9cf19efa7cc8213c73989ab0f22ef3
AlexProstolucky <alexprostolucky@gmail.com>
2023-04-20 20:28:30 +0300

    Добавьте файлы проекта.

---
+++ MIZ_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Immutable;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] arr = { 1, 1, 4, 5,2, 3, 6, 2, 4, 7, 3, 7, 8, 8, 9 };
+        var zap =  from i in arr 
+                  orderby i
+                  select i;
+        var res = zap.Distinct();
+
+        foreach (var i in res) 
+        {
+            Console.Write($"{i} ");
+        }
+    }
+}
